6f555b174f856970b9d99e5f8283abbe70fd18cf
Vidit Shah <0210vidit@gmail.com>
2020-06-20 22:12:12 +0530

    calc

--- Max/Phase1/Section2/assets/scripts/app.js
+++ Max/Phase1/Section2/assets/scripts/app.js
@@ -3,9 +3,10 @@ let currentResult = defaultresult;
 
 function add(num1, num2) {
   const result = num1 + num2;
+  return result;
 }
-
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+currentResult = add(3, 4);
+// currentResult = ((currentResult + 10) * 3) / 2 - 1;
 let calculationDescription = '(0+10) * 3/2-1';
 // let calculationDescriptionLiteral = `(${defaultresult}+10) * 3/2-1`;
 // let errroMessage = ' An Error Occured';
