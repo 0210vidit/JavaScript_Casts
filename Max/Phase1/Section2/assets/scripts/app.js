85076790fdd4835d6c40f9d6b3385e42e98d30a2
Vidit Shah <0210vidit@gmail.com>
2020-06-20 14:22:29 +0530

    defaultResult

--- Max/Phase1/Section2/assets/scripts/app.js
+++ Max/Phase1/Section2/assets/scripts/app.js
@@ -2,5 +2,6 @@ const defaultresult = 0;
 let currentResult = defaultresult;
 currentResult = ((currentResult + 10) * 3) / 2 - 1;
 let calculationDescription = '(0+10) * 3/2-1';
+let calculationDescriptionLiteral = `(${defaultresult}+10) * 3/2-1`;
 
 outputResult(currentResult, calculationDescription);
